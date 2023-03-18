8d5ce1d2b2d81138e29f29219aea4ebc187b1036
Zanatta97 <sa.zanatta@hotmail.com>
2023-03-17 21:46:18 -0300

    Aula 17/03/2022 com os arquivos certos

--- PSC01/src/main/R004.java
+++ PSC01/src/main/R004.java
@@ -1,10 +1,30 @@
 package main;
 
-public class Teste {
+import java.util.Scanner;
+
+public class R004 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Teste");
+		/*Faça um algoritmo que escreva todos os números
+		 * múltiplos de 3 entre 1 e N, sendo N um
+		 * valor digitado pelo usuário.
+		 */
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Informe um número qualquer: ");
+		
+		int numeroFinal = scanner.nextInt();
+		
+		for (int i = 1; i <= numeroFinal; ++i) {
+			//int resto = i % 3;
+			if (i % 3 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		scanner.close();
+
 	}
 
 }
